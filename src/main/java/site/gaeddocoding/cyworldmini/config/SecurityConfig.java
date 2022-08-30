a4636a45cbd8041db34939356b6ce5fce1beb15a
emperor-juwon <fwangjuwon@gmail.com>
2022-08-30 23:53:04 +0900

    security 인증필터 생성

---
+++ src/main/java/site/gaeddocoding/cyworldmini/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package site.gaeddocoding.cyworldmini.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder encode() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/s/**")
+                .authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login-form")
+                .defaultSuccessUrl("/");
+    }
+}
